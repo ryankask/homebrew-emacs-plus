acd1d7decb5d9a4381c16c3d0272d298db688364
Ryan Kaskel <dev@ryankaskel.com>
2026-06-01 17:39:23 +0100

    Add ns-win patch to emacs 32

--- Formula/emacs-plus@32.rb
+++ Formula/emacs-plus@32.rb
@@ -73,6 +73,7 @@ class EmacsPlusAT32 < EmacsBase
   local_patch "fix-ns-x-colors", sha: "9e5d3e26a8d388d3a000b697d582769645ca93ad597b4113744deba4b89a8b9e"
   local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
   local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
+  local_patch "ns-win", sha: "4829dfa8c447b714e010457f2da6b217be98bf53d454802c253bb6ae9da41038"
 
   #
   # Install
@@ -92,7 +93,7 @@ class EmacsPlusAT32 < EmacsBase
       --enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp
       --infodir=#{info}/emacs
       --prefix=#{prefix}
-      --with-native-compilation=aot
+      --with-native-compilation
     ]
 
     args << "--with-xml2"
